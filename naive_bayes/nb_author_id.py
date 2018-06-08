fc5ac1e8b56dd728101b3e022e141191825f4c72
jobo8505 <boegeholz.joern@gmail.com>
2018-06-08 14:21:20 +0200

    add requirements first tyr on naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,20 +14,20 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+t0 = time()
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
 
 
+print "training time:", round(time()-t0, 3), "s"
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+t0 = time()
+clf.predict()
+print "predict time:", round(time()-t0, 3), "s"
