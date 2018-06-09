657d2f132fc71273427417ea3feb410c5987f087
jboegeholz <boegeholz.joern@gmail.com>
2018-06-09 22:11:18 +0200

    add solution for svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,12 +7,13 @@
     Sara has label 0
     Chris has label 1
 """
-    
+from __future__ import print_function
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -20,11 +21,22 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
-### your code goes here ###
+print("Start training")
+t0 = time()
+clf = svm.SVC(kernel="linear")
+clf.fit(features_train, labels_train)
+print("training time:", round(time() - t0, 3), "s")
 
+print("start predicting")
+t0 = time()
+prediction = clf.predict(features_test)
+print("predict time:", round(time() - t0, 3), "s")
+
+# accuracy
+print("Calculating accuracy")
+accuracy = accuracy_score(labels_test, prediction)
+print("Accuracy calculated, and the accuracy is", accuracy)
 #########################################################
 
 
